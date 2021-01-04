01db5ebbee13ed03cf1ee24bdbb6fde353aaf3e8
Bartosz Lis <bartoszlis87@gmail.com>
2021-01-04 15:02:50 +0100

    add progress and overlay in devServer

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,26 @@ const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const path = require("path");
 const Compression = require("compression-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
-    entry: "./src/js/app.js",
+    entry: [
+        "./src/js/app.js",
+    //     './src/js/app.js',
+    //     './src/js/jquery.appear.min.js',
+    // './src/js/jquery.easing.1.3.js',
+    // './src/js/jquery.flexslider-min.js',
+    // './src/js/jquery.magnific-popup.js',
+    // './src/js/jquery.quicksand.js',
+    // './src/js/jquery.smooth-scroll.js',
+    //     './src/js/jquery.timer.js',
+    //    '/src/js/jquery-1.9.1.min.js',
+    //    './src/js/jquery-migrate-1.2.1.js',
+    //    './src/js/modernizr.custom.js',
+    //     './src/js/Placeholders.min.js',
+    //     './src/js/script.js',
+    ],
     output: {
         filename: "out.min.js",
         path: path.resolve(__dirname, "./build")
@@ -15,7 +32,8 @@ module.exports = {
     devtool: "source-map",
     watch: true,
     devServer: {
-        contentBase: path.join(__dirname, "./build/"),//??
+        contentBase: path.join(__dirname, "./build/"),
+        // inline: true, //automatyczny update
         publicPath: "/build/",
         compress: true,
         progress: true,
@@ -96,6 +114,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new MiniCSS({
             filename: "app.css",
         }),
@@ -107,5 +126,6 @@ module.exports = {
             threshold: 0,
             minRatio: 0.8
         })
+
     ]
 }
